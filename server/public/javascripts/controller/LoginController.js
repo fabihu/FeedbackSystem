ff4cd01ed2ebf80349c8b76112853f447e680f29
fabihu <fabian.huth@gmx.net>
2016-08-10 12:42:09 +0200

    added submit button functionality

--- server/public/javascripts/controller/LoginController.js
+++ server/public/javascripts/controller/LoginController.js
@@ -1,10 +1,39 @@
 FeedbackSystem.LoginController = (function() {
 	var that = {},
+	$submitButton = null,
+
 
 //Initialisierung der Logins
 	init = function() {
-		
+		console.log("LoginController init")
+		initControls();
+	},
+
+	initControls = function(){
+		$submitButton = $('#button-submit');
+		console.log($submitButton);
+		onClickButtonSubmit();
+	},
+
+	onClickButtonSubmit = function(){
+		$submitButton.on('click', function(e){
+			e.preventDefault();
+			if(checkLoginCredentials()){
+				$(location).attr('href', '/eval');
+	
+					$.get("/eval", function(data){
+							
+						});
+					
+			}
+		});
+
+	},
+
+	checkLoginCredentials = function(){
+		return true;		
 	};
+
 
 
 
