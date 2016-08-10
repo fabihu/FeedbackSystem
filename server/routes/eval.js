faaecc6ccd6701ca18407cce0d1d8803154fdd94
fabihu <fabian.huth@gmx.net>
2016-08-10 12:41:45 +0200

    added questionnaire template

--- server/routes/eval.js
+++ server/routes/eval.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-   res.send('endpoint for the evaluation');
+	console.log('redirecting to eval');
+  
+   return res.render('eval.ejs');
 });
 
 module.exports = router;
