dc2bc7b75afd3b5660cfec43cec1a1fbca978390
fabihu <fabian.huth@gmx.net>
2016-08-10 12:38:42 +0200

    changed route index to login

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 var eval = require ('./routes/eval');
 var survey = require ('./routes/survey');
@@ -16,7 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', login);
 app.use('/users', users);
 app.use('/eval', eval);
 app.use('/survey', survey);
