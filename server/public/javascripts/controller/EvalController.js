875c24c187b9b3cd76a054cf3f53c88ee67dc39d
fabihu <fabian.huth@gmx.net>
2016-09-12 10:22:22 +0200

    added controller for eval page removed unused index controller

---
+++ server/public/javascripts/controller/EvalController.js
@@ -0,0 +1,13 @@
+FeedbackSystem.EvalController = (function() {
+
+  var that = {},
+  init = function() {
+
+  	console.log("EvalController init");  	  	
+
+  };
+  
+  that.init = init;
+  return that;
+
+}());
