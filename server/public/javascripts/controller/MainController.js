6ba466761538d79b3e79114541f79913074235fd
fabihu <fabian.huth@gmx.net>
2016-09-16 12:03:13 +0200

    added controller

--- server/public/javascripts/controller/MainController.js
+++ server/public/javascripts/controller/MainController.js
@@ -3,8 +3,8 @@ FeedbackSystem.MainController = (function() {
 
 	//Initialisiert alle anderen notwendigen Controller 
 	init = function() {				
-		FeedbackSystem.LoginController.init();	
-					
+		FeedbackSystem.LoginController.init();
+		FeedbackSystem.EvalController.init();					
 	};
 
 	that.init = init;	
