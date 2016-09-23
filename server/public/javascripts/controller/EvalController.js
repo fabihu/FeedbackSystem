33aa49024598008948fb858aea4c8c3e6e5dd271
fabihu <fabian.huth@gmx.net>
2016-09-23 10:23:38 +0200

    added logic to get answers from forms

--- server/public/javascripts/controller/EvalController.js
+++ server/public/javascripts/controller/EvalController.js
@@ -1,32 +1,77 @@
 FeedbackSystem.EvalController = (function() {
 
   var that = {},
+  $buttonNext = null,
+  $containerQuestions = null,
+
   init = function() {
   	console.log("EvalController init");
-    //$(document).on('initEval', loadQuestions);   	  	
-
+    $(document).on("initControls", onInitControls);
+    $(document).on("fadeInContainer", onFadeInContainer)   
   },
 
-  initControls = function(){
-   
-    $containerQuestions = $('#container-questions');    
+  onInitControls = function(){
+    console.log("trigger init controls")   
+    $containerQuestions = $('#container-questions');
+    $buttonNext = $("button[name=button-next");
+    
+    getSelectedAnswers();
     
   },
 
-  loadQuestions = function(){
-    console.log("load questions");
-    $.post("/get-questions/", function(data){
-     
-    });
-  
-},
+  onFadeInContainer = function(){
+    console.log("fade in");
+  //  $containerQuestions.addClass("animated fadeIn");
+  }
 
-  loadAnswers = function(){
-  	
+  getSelectedAnswers = function(){
+  var collection_answers = [];
+
+  $buttonNext.click(function(){
+
+  var question_id =  $(this).attr("question-id");
+  var question_type = $(this).attr("question-type");
+        
+  if(question_type == 0) {
+    var $checkboxAnswer = $("input[name=answer-check-radio-"+ question_id+"]");  
+    $.each($checkboxAnswer, function(index, box){
+      if (box.checked){
+        var answer = {
+          question_id: $(box).attr('question-id'),
+          option: $(box).attr('answer-id'),
+          type: 0,
+          value: true
+        };        
+        collection_answers.push(answer);
+      }
+    });
+  } else {
+
+    var $radioGroup = $('#container-questions-' + question_id);    
+    var $radioAnswers = $radioGroup.find("input")    
+  
+    $.each($radioAnswers, function(index, radio){
+          if (radio.checked){
+            var answer = {
+              question_id: $(radio).attr('question-id'),
+              option: $(radio).attr('answer-id'),
+              type: 1,
+              value: radio.value
+            };        
+            collection_answers.push(answer);
+          }
+    });
+  }
+  console.log(collection_answers);
+  });
+
+  
+
+
   };
   
   that.init = init;
-  that.initControls = initControls;
+  that.onInitControls = onInitControls;
   return that;
 
 }());
