53b1588cb7472cdaf24a1e66a835730722bc96be
fabihu <fabian.huth@gmx.net>
2016-10-24 10:07:58 +0200

    added db methods to add update question order in assignmnt

--- server/db/dbhandler.js
+++ server/db/dbhandler.js
@@ -108,12 +108,13 @@ getQuestionsForTrips = function(trips, callback){
 getQuestionsForTrip  = function(trip_id, callback){
   pool.getConnection(function(err, connection) {     
   async.waterfall([function (callback){    
-    connection.query('SELECT * FROM ' + TABLE_QUESTION_SET + ' WHERE trip_id = ' + trip_id, function(err, dbResponse) {
+   connection.query('SELECT * FROM ' + TABLE_QUESTION_SET + ' WHERE trip_id = ? ORDER BY position ASC' , [trip_id], function(err, dbResponse) {      
       if(err) {
         console.log(err);       
-      }
+      }    
       callback(null, dbResponse);    
     });
+   
 
   }, function (result, callback){
     
@@ -140,8 +141,18 @@ getQuestionsForTrip  = function(trip_id, callback){
  
 }
 
-getAnswersForTrip  = function(trip_id, callback){
-
+getActiveAnswersFromCollection  = function(question_id, callback){
+pool.getConnection(function(err, connection) {
+  
+var query = connection.query('SELECT * FROM ' + TABLE_ANSWER_COLLECTION + ' WHERE question_id = ? AND flag_history="active"', [question_id], function(err, dbResponse) {
+    
+  if(err)console.log(err);
+  callback(null, dbResponse);    
+  
+  connection.release();
+  
+}); 
+});
 }
 
 getAnswers = function(callback){
@@ -245,6 +256,7 @@ insertStandardQuestionsIntoSet = function(callback){
        callback(err);
     });
 } 
+
 insertStandardAnswersIntoSet = function(outerCallback){
 
   async.timesSeries(6, function(n, callback) {
@@ -259,7 +271,7 @@ insertStandardAnswersIntoSet = function(outerCallback){
     console.log("Answer ID Array: " + answer_ids);
     
     async.eachSeries(answer_ids, function(id, innerCallback){
-          insertAnswersIntoSet(id, function(err){
+          insertAnswersIntoSet(id, n, function(err){
             console.log(id + " inserted into database");
             innerCallback(err);
           });
@@ -278,16 +290,16 @@ insertStandardAnswersIntoSet = function(outerCallback){
 
 insertQuestionsIntoSet = function(id, callback){
   pool.getConnection(function(err, connection) {
-   var query = connection.query('INSERT INTO ' + TABLE_QUESTION_SET + ' (trip_id, question_id, question_version) VALUES ((SELECT max(id) FROM trip), ' + (id + 1) + ', (SELECT max(question_version) FROM '+TABLE_QUESTION_COLLECTION + ' WHERE question_id=' + (id + 1) +'))', function(err) {    
+   var query = connection.query('INSERT INTO ' + TABLE_QUESTION_SET + ' (trip_id, position, question_id, question_version) VALUES ((SELECT max(id) FROM trip), '+ (id + 1) + ', ' + (id + 1) + ', (SELECT max(question_version) FROM '+TABLE_QUESTION_COLLECTION + ' WHERE question_id=' + (id + 1) +'))', function(err) {    
            if(err)console.log(err);
            callback(err);
    });
   }); 
 }
 
-insertAnswersIntoSet = function(id, callback){
+insertAnswersIntoSet = function(id, question_id, callback){
   pool.getConnection(function(err, connection) {
-   var query = connection.query('INSERT INTO ' + TABLE_ANSWER_SET + ' (trip_id, answer_id, answer_version) VALUES ((SELECT max(id) FROM trip), ' + id + ', (SELECT max(answer_version) FROM '+TABLE_ANSWER_COLLECTION + ' WHERE answer_id=' + id +'))', function(err) {
+   var query = connection.query('INSERT INTO ' + TABLE_ANSWER_SET + ' (trip_id, question_id, answer_id, answer_version) VALUES ((SELECT max(id) FROM trip), ' + (question_id+1) + ',' + id + ', (SELECT max(answer_version) FROM '+TABLE_ANSWER_COLLECTION + ' WHERE answer_id=' + id +'))', function(err) {
     if(err)console.log(err);
     callback(err);
    });
@@ -519,15 +531,115 @@ updateAnswer = function(answer_id, data, callback){
   });  
 }
 
-changeActiveQuestion = function(question_id, question_status, callback){
+updateActiveFlagTrip = function(trip_id, trip_status, callback){
   pool.getConnection(function(err, connection) {
-  var active = (question_status == "true") ? 1:0;
+  var active = (trip_status == "true") ? 1:0; 
   
-  var query = connection.query('UPDATE ' + TABLE_QUESTIONS + ' SET flag_active=? WHERE id=?' , [active, question_id], function(err, result) {
-  
+  var query = connection.query('UPDATE ' + TABLE_TRIPS + ' SET flag_active=? WHERE id=?' , [active, trip_id], function(err, result) {  
     if(err){
       console.log(err);
     }
+
+    getQuestionsForTrip(trip_id, function(err, dbQuestions){
+
+      async.mapSeries(dbQuestions, function(question, callback){        
+        var question_id = question[0].question_id;       
+        getActiveAnswersFromCollection(question_id, function(err, dbAnswers){          
+        
+          async.eachSeries(dbAnswers, function(answer, callback){           
+            updateAnswerSet(trip_id, answer, function(err){
+             if(err) console.log(err);
+             callback();
+            })
+
+          }, function(err){
+             if(err) console.log(err);
+            callback();
+          });
+        })
+       },
+       function(err, result){
+        if(err)console.log(err);       
+        callback(result)
+      });      
+    });
+
+
+  });        
+  connection.release();
+
+  });
+}
+
+
+updateAnswerSet = function(trip_id, answer, callback){
+ var query =  pool.getConnection(function(err, connection) {
+
+  var question_id = answer.question_id;
+  var answer_id = answer.answer_id;
+  var answer_version = answer.answer_version; 
+
+
+  connection.query('DELETE FROM ' + TABLE_ANSWER_SET + ' WHERE trip_id=? AND question_id=? AND answer_id=? AND answer_version=?' , [trip_id, question_id, answer_id, answer_version], function(err, result) {  
+    if(err){
+      console.log(err);
+    }
+      var query = connection.query('INSERT INTO ' + TABLE_ANSWER_SET + ' (trip_id, question_id, answer_id, answer_version) VALUES (?,?,?,?)' , [trip_id, question_id, answer_id, answer_version], function(err, result) {  
+        if(err){
+          console.log(err);
+        }
+    
+      });
+
+  });
+   
+  callback(err);    
+  connection.release();
+  //console.log(query.sql);
+  }); 
+
+}  
+
+updateOrderQuestionSet = function(trip_id, question_id, position, callback){
+ var query =  pool.getConnection(function(err, connection) {  
+  var query = connection.query('UPDATE ' + TABLE_QUESTION_SET + ' SET position=? WHERE trip_id=? AND question_id=?' , [position, trip_id, question_id], function(err, result) {  
+    if(err){
+      console.log(err);
+    }
+
+  });
+   
+  callback(err);    
+  connection.release();
+
+  }); 
+}
+
+insertIntoQuestionSet = function(trip_id, question_id, question_version, callback){
+  pool.getConnection(function(err, connection) {  
+  var query = connection.query('INSERT INTO ' + TABLE_QUESTION_SET + ' (trip_id, position, question_id, question_version) VALUES (?, (SELECT max(QS.position) FROM '+TABLE_QUESTION_SET+' QS WHERE QS.trip_id=?), ?,?)' , [trip_id, trip_id, question_id, question_version], function(err, result) {  
+    if(err){
+      console.log(err);
+    }
+  });  
+  callback(err);    
+  connection.release();
+
+  });
+}
+
+deleteFromQuestionSet = function(trip_id, question_id, question_version, callback){  
+ pool.getConnection(function(err, connection) {  
+  var query = connection.query('DELETE FROM ' + TABLE_QUESTION_SET + ' WHERE trip_id=? AND question_id=? AND question_version=?' , [trip_id, question_id, question_version], function(err, result) {  
+    if(err){
+      console.log(err);
+    }
+      connection.query('DELETE FROM ' + TABLE_ANSWER_SET + ' WHERE trip_id=? AND question_id=?', [trip_id, question_id], function(err, result) {  
+      if(err){
+          console.log(err);
+        }
+      });
+       
   });  
   callback(err);    
   connection.release();
@@ -539,7 +651,9 @@ exports.init = init;
 exports.checkLoginCredentials = checkLoginCredentials;
 exports.getQuestions = getQuestions;
 exports.getQuestionsForTrips = getQuestionsForTrips;
+exports.getQuestionsForTrip = getQuestionsForTrip;
 exports.getAllActiveQuestions = getAllActiveQuestions;
+//exports.getActiveAnswersFromCollection = getActiveAnswersFromCollection;
 exports.getAnswers = getAnswers;
 exports.getTrips = getTrips;
 exports.insertUserAnswers = insertUserAnswers;
@@ -549,9 +663,12 @@ exports.getAnswersForQuestion = getAnswersForQuestion;
 exports.getTravelTypes = getTravelTypes;
 exports.deleteTrip = deleteTrip;
 exports.updateTrip = updateTrip;
-exports.changeActiveQuestion = changeActiveQuestion;
+exports.updateActiveFlagTrip = updateActiveFlagTrip;
 exports.insertNewAnswerOptions = insertNewAnswerOptions;
 exports.deleteQuestion = deleteQuestion;
 exports.updateQuestion = updateQuestion;
+exports.updateOrderQuestionSet = updateOrderQuestionSet
+exports.insertIntoQuestionSet = insertIntoQuestionSet;
+exports.deleteFromQuestionSet = deleteFromQuestionSet;
 exports.updateAnswer = updateAnswer;
 exports.deleteAnswer = deleteAnswer;
