d55bf07f27d77b34450cc57a620c429103e4dae2
fabihu <fabian.huth@gmx.net>
2016-11-04 11:16:55 +0100

    removed pw changed layout

--- server/public/javascripts/controller/LoginController.js
+++ server/public/javascripts/controller/LoginController.js
@@ -12,33 +12,42 @@ FeedbackSystem.LoginController = (function() {
 	},
 
 	initControls = function(){
-		$submitButton = $('#button-submit');		
+		$submitButton = $('#button-submit');
+		$('#container-info').addClass('animated slideInDown');
+		$('#container-login-controls').addClass('animated slideInDown');
+				
 		onClickButtonSubmit();
 	},
 
 	onClickButtonSubmit = function(){
 		$txtUserTravelID = $('#InputTravelId');
-		$txtUserPassword = $('#InputTravelPass');
+		//$txtUserPassword = $('#InputTravelPass');
 		
 		$submitButton.on('click', function(e){
 			e.preventDefault();
 			var user_id = $txtUserTravelID.val();
-			var user_password = $txtUserPassword.val()
+			//var user_password = $txtUserPassword.val()
 			
-			$.post("/check-login/", { id: user_id, password: user_password }, function(data){							
-     			
+			//$.post("/check-login/", { id: user_id, password: user_password }, function(data){
+			$.post("/check-login/", {id: user_id}, function(data){							
       			var credentials_ok = data;
 				if(credentials_ok){
-					$.post('/get-questions/',{id: user_id}, function( data ) {
-
-						$('#main-container').empty();
-						$('#main-container').append(data);
-						$(document).trigger('initControls');
-						$(document).trigger('fadeInContainer');
-						$(document).trigger('getTripId', user_id);						
-					});
+					var is_open = data.status;
+					if(is_open == 0){
+						$.post('/get-questions/',{id: user_id}, function( data ) {
+	
+								$('#main-container').empty();
+								$('#header-container').append(data);
+								$(document).trigger('initControls');
+								$(document).trigger('fadeInContainer');
+								$(document).trigger('getTripId', user_id);						
+						});
+					} else {
+					window.alert("Reise noch nicht zur Bewertung freigegeben!");
+					}
+     				
 				} else {
-					window.alert("Reise-ID oder Passwort ungültig");
+					window.alert("Reise-ID nicht vorhanden!");
 				}
       		});					
 			
