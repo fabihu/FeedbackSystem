b93070f04bf250c122c3b18d6690d50eb6f23aa6
fabihu <fabian.huth@gmx.net>
2016-11-04 11:13:47 +0100

    added error trace

--- server/app.js
+++ server/app.js
@@ -31,8 +31,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found');  
   err.status = 404;
+  console.log(req.url); 
   next(err);
 });
 
